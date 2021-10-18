5990fa3937faa8063df92041472a3cf9550966a7
Knut Melvær <knut.melvaer@gmail.com>
2021-10-18 16:54:53 +0200

    tweak preview stuff

--- web/src/_data/posts.js
+++ web/src/_data/posts.js
@@ -1,20 +1,40 @@
+const blocksToHtml = require('@sanity/block-content-to-html')
 const client = require('../sanity')
+const isServerless = process.env.ELEVENTY_SERVERLESS
 
-const query = `//groq
-  *[_type == "post" && publishedAt < now()]{
+const previewQuery = `//groq
+  *[_type == "post" && _id in path("drafts.**")]{
+    _id,
     title,
     publishedAt,
+    body,
     "slug": slug.current,
-    "body": pt::text(body)
+  }
+`
+
+const query = `//groq
+  *[_type == "post" && publishedAt < now() && !(_id in path("drafts.**"))]{
+    _id,
+    title,
+    publishedAt,
+    body,
+    "slug": slug.current,
   }
 `
 
 async function getPosts() {
-  const posts = await client.fetch(query)
-  return posts.map(post => ({
-    ...post,
-    publishedAt: new Date(post.publishedAt).toLocaleDateString(),
-  }))
+  console.log('isServerless', isServerless)
+  const posts = await client.fetch(!isServerless ? query : previewQuery)
+  return posts
+    .map(post => ({
+      ...post,
+      body: blocksToHtml({
+        blocks: post.body,
+        imageOptions: { w: 320, h: 240, fit: 'max' },
+        ...client.config()
+      }),
+      publishedAt: new Date(post.publishedAt).toLocaleDateString(),
+    }))
 }
 
 module.exports = getPosts
--- web/src/sanity.js
+++ web/src/sanity.js
@@ -4,7 +4,8 @@ const client = sanityClient({
   projectId: 'dow9xz1g',
   dataset: 'production',
   apiVersion: '2021-10-18',
-  useCdn: false
+  useCdn: false,
+  token: process.env.SANITY_READ_TOKEN
 })
 
 module.exports = client
