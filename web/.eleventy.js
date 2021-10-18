1e7e901163d0ce82e4e3784384acd37410922285
Knut Melvær <knut.melvaer@gmail.com>
2021-10-18 18:03:53 +0200

    copy sanity config

--- web/.eleventy.js
+++ web/.eleventy.js
@@ -3,7 +3,8 @@ const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy')
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
     name: 'preview',
-    functionsDir: './netlify/functions/'
+    functionsDir: './netlify/functions/',
+    copy: ['src/sanity.js']
   })
   return {
     dir: {
